2a498dd0a6a5edff5d3db8e77637ea0d637f89da
Bruno Harari <hararibruno@gmail.com>
2022-04-20 12:30:38 -0300

    first commit

--- js/modal.js
+++ js/modal.js
@@ -1,37 +1,37 @@
-$(document).ready(function () {
-  $('#product-image').click(function () {
-    $('#modal4').modal('show');
-  });
-});
-
-
-
-
-
-
-$('#modal4').on('show.bs.modal', function (event) {
-  let img = $(event.relatedTarget) // Image that triggered the modal
-
-  let title = img.data('title')
-  console.log(title)
-  let description = img.data('description')
-  let productImg = img.data('img')
-  let image = document.getElementById('productImage')
-  let productUrl = img.data('url')
-  let spanName = document.getElementById('product-modal-name')
-  let productButton = document.getElementById('product-button')
-
-  // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  let modal = $(this)
-  modal.find('.modal-title').text(title)
-  productButton.href = productUrl
-  spanName.innerText = title
-  image.src = productImg
-  modal.find('.modal-body #description').text(description)
-})
-
-// $('#modal-container').click(function () {
-//   $('body').removeClass('modal-active');
+$(document).ready(function () {
+  $('#product-image').click(function () {
+    $('#modal4').modal('show');
+  });
+});
+
+
+
+
+
+
+$('#modal4').on('show.bs.modal', function (event) {
+  let img = $(event.relatedTarget) // Image that triggered the modal
+
+  let title = img.data('title')
+  console.log(title)
+  let description = img.data('description')
+  let productImg = img.data('img')
+  let image = document.getElementById('productImage')
+  let productUrl = img.data('url')
+  let spanName = document.getElementById('product-modal-name')
+  let productButton = document.getElementById('product-button')
+
+  // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  let modal = $(this)
+  modal.find('.modal-title').text(title)
+  productButton.href = productUrl
+  spanName.innerText = title
+  image.src = productImg
+  modal.find('.modal-body #description').text(description)
+})
+
+// $('#modal-container').click(function () {
+//   $('body').removeClass('modal-active');
 // });
