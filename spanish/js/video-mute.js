0a0060b805622a3d6c069bd4910c61a868454187
Bruno Harari <hararibruno@gmail.com>
2022-05-10 19:28:18 -0300

    mute videos

--- spanish/js/video-mute.js
+++ spanish/js/video-mute.js
@@ -1,27 +1,35 @@
-function checkVisible(elm) {
-  var rect = elm.getBoundingClientRect();
-  var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-  return !(rect.bottom < 0 || rect.top - viewHeight + 50 >= 0);
-
-}
-
-function playVid() {
-  console.log('playing')
-  video.play();
-}
-
-function pauseVid() {
-  console.log('paused')
-    video.pause();
-  
-
-}
-
-
-window.onscroll = function () {
-  // CODE
-  const video = document.getElementById('video')
-
-  video.muted = !checkVisible(video);
-  video.muted ? pauseVid() : playVid()
+function checkVisible(elm) {
+  var rect = elm.getBoundingClientRect();
+
+  var viewHeight = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight
+  );
+  
+
+  return !(rect.bottom < 100 || rect.top - viewHeight >= 0);
+}
+
+function playVid() {
+  console.log("playing");
+
+  video.volume = 0.1;
+
+  video.play();
+}
+
+function pauseVid() {
+  console.log("paused");
+
+  video.pause();
+}
+
+window.onscroll = function () {
+  // CODE
+
+  const video = document.getElementById("video");
+  video.volume = 0.1;
+  video.muted = !checkVisible(video);
+
+  video.muted ? pauseVid() : playVid();
 };
